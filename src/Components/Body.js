766171f821276a2053585f0012203e8d309437e1
Anshala Gupta <anshala.gupta@streebo.com>
2025-03-09 17:01:44 +0530

    adding

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,21 @@
 import resList from "../Utility/MockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
 const Body =()=> {
-  
+    const [restaurantList,setRestaurantList]=useState(resList);
+    const resFilter= ()=>{
+      const filterRes= restaurantList.filter((res)=>res.info.avgRating>4);
+      setRestaurantList(filterRes);
+     
+}
     return(
        <div className="body">
-         <div className="search">Search</div>
+         <div className="BtnDiv">
+            <button onClick={resFilter}>Filter 4+ rated Restaurants</button>
+         </div> 
          <div className="res-container">
-         {resList.map((restaurant)=>  <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)}
+         {restaurantList.map((restaurant)=>  <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)}
            </div>
        </div>
     )
